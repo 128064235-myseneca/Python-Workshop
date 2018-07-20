8151c6d985ec52fe977e0d8a2d37d2595a754239
joshi-shubham <40904113+joshi-shubham@users.noreply.github.com>
2018-07-20 19:41:43 +0545

    Add files via upload

---
+++ shubham_Q1-4Q1.py
@@ -0,0 +1,19 @@
+v = 25
+u = 0
+t = 10
+
+a = (v-u)/t
+
+print ("the acceleration is ",a,end=" m/s2 \n")  # acceleration
+
+f_name = input("Enter your first name:: ")
+l_name = input("Ebter your last name:: ")
+
+print("Your name is ",f_name+" "+l_name)  # print name by concatination
+
+rand = input("enter a string :: ")
+print((rand+" ")*10)  # print a string 10 times
+
+strn = input("enter any string")
+print(strn,end="**")  # overriding ending argument
+
---
+++ shubham_Q5.py
@@ -0,0 +1,24 @@
+"""
+10 built in functions
+"""
+a = -5
+print(abs(a))  # prints the absolute value
+
+print(ord('#'))  # prints the ascii value
+print(hex(15))  # prints the hexadecimal value of 15
+
+print(chr(5565))  # prints the character of ascii value 5565
+
+a="nepal"
+print(len(a))  # prints the lenght of the string 'a'
+
+print(divmod(5,2))  # prints the division and modulus
+
+print(pow(5,10))  # prints the 5 to the power 10
+
+print(type(a))  # prints the data type of a
+
+x= "5+6+7"
+print(eval(x))  # evaluates and prints the mathematical operation present in the string
+
+print(oct(9))  # prints the octal value of 9
